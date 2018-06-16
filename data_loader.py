e3602f1dd778a6d0fd95b253d3c68323429a683d
kh-mo <momkdh2@naver.com>
2018-06-16 16:57:33 +0900

    init

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from nltk.tokenize import word_tokenize
 
 # CNN_stories폴더와 DM_stories 폴더 내 파일 숫자
 num_expected_cnn_stories = 92579
@@ -17,6 +18,20 @@ def check_num_stories(stories_dir, num_expected):
         raise Exception("stories directory %s contains %i files but should contain %i" %
                         (stories_dir, num_stories, num_expected))
 
+# 파일들을 tokenize화 하여 새 폴더에 저장
+def tokenize_stories(stories_dir, tokenized_stories_dir):
+    stories = os.listdir(stories_dir)
+    print("start tokenizing")
+    for file in stories:
+        with open(stories_dir + "/" + file, encoding='UTF8') as f:
+            file_content = f.readlines()
+            tokenized_content = []
+            for i in file_content:
+                tokenized_content += word_tokenize(i) + ["\n"]
+            with open(tokenized_stories_dir + "/" + file, "w", encoding='UTF8') as s:
+                s.write(' '.join(tokenized_content))
+    print("finish tokenizing")
+
 if __name__ == '__main__':
     # sys.argv[0]는 프로그램 명인 data_loader
     if len(sys.argv) != 3:
@@ -33,3 +48,7 @@ if __name__ == '__main__':
     if not os.path.exists(cnn_tokenized_stories_dir): os.makedirs(cnn_tokenized_stories_dir)
     if not os.path.exists(dm_tokenized_stories_dir): os.makedirs(dm_tokenized_stories_dir)
     if not os.path.exists(finished_files_dir): os.makedirs(finished_files_dir)
+
+    # Run stanford tokenizer on both stories dirs, outputting to tokenized stories directories
+    tokenize_stories(cnn_stories_dir, cnn_tokenized_stories_dir)
+    tokenize_stories(dm_stories_dir, dm_tokenized_stories_dir)
